815727bc98b9bde3028753d6bb10ab581acf5f75
Oscar <oscar2412086@hybridge.education>
2026-01-20 16:02:05 -0600

    Entregando ejercisios

--- ejercicio01.js
+++ ejercicio01.js
@@ -1,8 +1,13 @@
 // Ejercicio 1: Declaración de Función
-// Modifica la siguiente función creando una nueva variable llamada nombre y asignándole tu nombre. La función debe retornar esta nueva variable que contiene tu nombre
+// Modifica la siguiente función creando
+//  una nueva variable llamada nombre y 
+// asignándole tu nombre. La función debe retornar 
+// esta nueva variable que contiene tu nombre
 
 function obtenerNombre() {
     // Escribe tu código aquí
+    let nombre = "Oscar";
+    return nombre;
 }
 
 // No modificar.
